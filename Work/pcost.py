b2ab1643a017e2031ef64ce97e37ae109baebf32
ReadingShades <42413498+ReadingShades@users.noreply.github.com>
2020-09-25 17:36:06 -0500

    Update pcost.py
    Lesson 1 file exercise s

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+with open('./Data/portfolio.csv','rt') as f:
+    headers = next(f).split(',')
+    print(headers)
+    total = 0
+    for line in f:
+        row = line.split(',')
+        print(row)  
+        total += float(row[1]) * float(row[2])
+    print('The total inventory value is: ',total)
+
+if f.closed:
+    print('file is closed')
+else:
+    print('file is open')
