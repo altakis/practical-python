b4c4cfee97f31ea662c72b787167706045daff54
ReadingShades <42413498+ReadingShades@users.noreply.github.com>
2020-09-28 21:03:43 -0500

    misc
    completed pcost.py exercises and changed formatting setting to black

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,17 +1,33 @@
 # pcost.py
 #
 # Exercise 1.27
-with open('./Data/portfolio.csv','rt') as f:
-    headers = next(f).split(',')
-    print(headers)
-    total = 0
-    for line in f:
-        row = line.split(',')
-        print(row)  
-        total += float(row[1]) * float(row[2])
+import sys
+import csv
+def portfolio_cost(filename):
+    'Returns the total value of an inventory of the name,shares,cost format'
+    with open(filename,'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        print(headers)
+        total = 0
+        for row in rows:
+                print(row)
+                # line = row.split(',')                
+                try:              
+                    total += float(row[1]) * float(row[2])
+                except ValueError:
+                    print("\nCouldn't parse, please sanitize file.\nError line:",row,"\n")            
+
     print('The total inventory value is: ',total)
 
-if f.closed:
-    print('file is closed')
+    if f.closed:
+        print('file is closed')
+    else:
+        print('file is open')
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
 else:
-    print('file is open')
+    filename = './Data/portfolio.csv'
+
+portfolio_cost(filename)
